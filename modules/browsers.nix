3838257919c23982924a1875baa8c48373555975
winstonwumbo <wjw5326@psu.edu>
2025-06-22 03:21:45 -0400

    Experiment with brave-flags.conf (Chromium args are broken with NixGL currently)

--- modules/browsers.nix
+++ modules/browsers.nix
@@ -1,10 +1,14 @@
 { config, pkgs, ... }:
 {
-  programs.chromium = {
+  programs.brave = {
     enable = true;
     package = (config.lib.nixGL.wrap pkgs.brave);
     extensions = [
       { id = "ddkjiahejlhfcafbddmgiahcphecmpfh"; } # ublock origin
     ];
-  }
+  };
+
+  xdg.configFile."brave-flags.conf" = {
+    source = config.lib.file.mkOutOfStoreSymlink "../dotfiles/brave-flags.conf";
+  };
 }
