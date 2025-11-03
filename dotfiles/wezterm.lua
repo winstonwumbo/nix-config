a7f9463dc5accaf9ed35c456806fcbe52d6b1309
winstonwumbo <wjw5326@psu.edu>
2025-11-03 14:45:12 -0500

    Fish shell aliases (update, upgrade, etc.), dbus symlink for Tangram, wezterm copy macro

--- dotfiles/wezterm.lua
+++ dotfiles/wezterm.lua
@@ -1,5 +1,6 @@
 -- Pull in the wezterm API
 local wezterm = require 'wezterm'
+local act = wezterm.action
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
@@ -16,6 +17,22 @@ config.enable_scroll_bar = true
 config.scrollback_lines = 10000
 config.hide_mouse_cursor_when_typing = false
 config.window_close_confirmation = 'NeverPrompt'
+
+config.mouse_bindings = {
+	{
+		event = { Down = { streak = 1, button = "Right" } },
+		mods = "NONE",
+		action = wezterm.action_callback(function(window, pane)
+			local has_selection = window:get_selection_text_for_pane(pane) ~= ""
+			if has_selection then
+				window:perform_action(act.CopyTo("ClipboardAndPrimarySelection"), pane)
+				window:perform_action(act.ClearSelection, pane)
+			else
+				window:perform_action(act({ PasteFrom = "Clipboard" }), pane)
+			end
+		end),
+	},
+}
 
 -- and finally, return the configuration to wezterm
 return config
--- home.nix
+++ home.nix
@@ -17,8 +17,6 @@
 
     # You can also split up your configuration and import pieces of it here:
     # ./nvim.nix
-    #    ./modules/helix.nix
-    ./modules/browser.nix
     ./modules/flatpak.nix
     ./modules/terminal.nix
     ./modules/vscode.nix
@@ -30,7 +28,7 @@
   home.homeDirectory = "/var/home/ruyu";
 
   # This value determines the Home Manager release that your configuration is
-  # compatible with. You should not change this value, even if you update Home Manager. 
+  # compatible with. You should not change this value, even if you update Home Manager.
   # If you do want to update the value, then make sure to first check the release notes.
   home.stateVersion = "25.05"; # Please read the comment before changing.
 
@@ -46,7 +44,7 @@
   # environment.
   home.packages =
     (with pkgs; [
-      # Gnome config
+      # Gnome shell theme
       (pkgs.marble-shell-theme.override {
         colors = [ "blue" ];
         additionalInstallationTweaks = [
@@ -63,7 +61,7 @@
       nerd-fonts.jetbrains-mono
       gnome-tweaks
 
-      # Desktop extensions
+      # Gnome extensions
       gnomeExtensions.user-themes
       gnomeExtensions.dash-to-panel
       gnomeExtensions.arcmenu
@@ -77,7 +75,8 @@
       # gtk-engine-murrine
       # sassc
 
-      # CLI utils
+      # Terminal tools
+      (config.lib.nixGL.wrap wezterm)
       yt-dlp
       wgcf
       unimatrix
@@ -86,13 +85,20 @@
       p7zip
 
       # Dev tools
+      jetbrains-toolbox
+      gh
       mise
       gcc
       gdb
-      ghidra
-      turso-cli
+      cppcheck
       sqlite
-      (config.lib.nixGL.wrap pkgs.warp-terminal)
+      turso-cli
+      (config.lib.nixGL.wrap warp-terminal)
+      
+      # Sec tools
+      ghidra
+      detect-it-easy
+      zap
 
       # Container tools
       flatpak-builder
@@ -100,16 +106,80 @@
       docker-compose
       kubectl
       kind
-
-      nil
+      nixd
       nixfmt-rfc-style
     ])
     ++ (with pkgs-stable; [
       # Packages that break with nightly
-      jetbrains.clion
-      jetbrains.rust-rover
       vagrant
     ]);
+
+  # Fish shell configuration
+  programs.fish = {
+    enable = true;
+    shellAliases = {
+      nix-update = "cd ${config.home.homeDirectory}/.config/nix-config && nix flake update && cd -";
+      nix-upgrade = "home-manager switch"; # && nix-collect-garbage --delete-older-than 14d
+      nix-edit = "code ${config.home.homeDirectory}/.config/nix-config";
+    };
+    shellInit = ''
+      set fish_greeting # Disable greeting
+
+      mise activate fish | source
+
+      fish_add_path --global ~/.yarn/bin
+    '';
+  };
+
+  # If you don't want to manage your shell
+  # through Home Manager then you have to manually source 'hm-session-vars.sh'
+  # located at either
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  home.sessionVariables = {
+    # EDITOR = "emacs";
+    DOCKER_CONFIG = "${config.home.homeDirectory}/.config/docker";
+    NIXOS_OZONE_WL = "1";
+    SSH_AUTH_SOCK = "${config.home.homeDirectory}/.var/app/com.bitwarden.desktop/data/.bitwarden-ssh-agent.sock";
+  };
+
+  # # It is sometimes useful to fine-tune packages, for example, by applying
+  # # overrides. You can do that directly here, just don't forget the
+  # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
+  # # fonts?
+  # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+
+  # # You can also create simple shell scripts directly inside your
+  # # configuration. For example, this adds a command 'my-hello' to your
+  # # environment:
+  # (pkgs.writeShellScriptBin "my-hello" ''
+  #   echo "Hello, ${config.home.username}!"
+  # '')
+
+  # Gnome theme and settings 
+  gtk = {
+    enable = true;
+
+    # theme = {
+    #   name = "Graphite-blue-compact-nord";
+    #   package = (pkgs.graphite-gtk-theme.override {
+    #     themeVariants = [ "blue" ];
+    #     colorVariants = [ "standard" ];
+    #     sizeVariants = [ "compact" ];
+    #     tweaks = [ "normal" "nord"];
+    #   });
+    # };
+
+  theme = {
+      name = "adw-gtk3";
+      package = pkgs.adw-gtk3;
+    };
+
+    iconTheme = {
+      name = "Numix-Circle";
+      package = pkgs.numix-icon-theme-circle;
+    };
+  };
 
   dconf.settings = {
     # ...
@@ -130,59 +200,7 @@
     };
   };
 
-  programs.fish = {
-    enable = true;
-    shellAliases = {
-      nix-upgrade = "home-manager switch"; #  && nix-collect-garbage --delete-older-than 14d
-      nix-edit = "code ${config.home.homeDirectory}/.config/nix-config";
-    };
-    shellInit = ''
-      set fish_greeting # Disable greeting
-
-      mise activate fish | source
-    '';
-  };
-
-  # # It is sometimes useful to fine-tune packages, for example, by applying
-  # # overrides. You can do that directly here, just don't forget the
-  # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-  # # fonts?
-  # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-  # # You can also create simple shell scripts directly inside your
-  # # configuration. For example, this adds a command 'my-hello' to your
-  # # environment:
-  # (pkgs.writeShellScriptBin "my-hello" ''
-  #   echo "Hello, ${config.home.username}!"
-  # '')
-
-
-  gtk = {
-    enable = true;
-
-    # theme = {
-    #   name = "Graphite-blue-compact-nord";
-    #   package = (pkgs.graphite-gtk-theme.override {
-    #     themeVariants = [ "blue" ];
-    #     colorVariants = [ "standard" ];
-    #     sizeVariants = [ "compact" ];
-    #     tweaks = [ "normal" "nord"];
-    #   });
-    # };
-
-    theme = {
-      name = "adw-gtk3-dark";
-      # package = (pkgs.orchis-theme.override {
-      #   tweaks = [ "solid" "primary"];
-      # });
-    };
-
-    iconTheme = {
-      name = "Numix-Circle";
-      package = pkgs.numix-icon-theme-circle;
-    };
-  };
-
+  # My symlinks
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
@@ -190,26 +208,36 @@
     # # the Nix store.
     ".local/share/icons/Numix".source = "${pkgs.numix-icon-theme-circle}/share/icons/Numix";
     ".local/share/icons/Numix-Light".source = "${pkgs.numix-icon-theme-circle}/share/icons/Numix-Light";
-    ".local/share/icons/Numix-Circle".source = "${pkgs.numix-icon-theme-circle}/share/icons/Numix-Circle";
-    ".local/share/icons/Numix-Circle-Light".source = "${pkgs.numix-icon-theme-circle}/share/icons/Numix-Circle-Light";
+    ".local/share/icons/Numix-Circle".source =
+      "${pkgs.numix-icon-theme-circle}/share/icons/Numix-Circle";
+    ".local/share/icons/Numix-Circle-Light".source =
+      "${pkgs.numix-icon-theme-circle}/share/icons/Numix-Circle-Light";
+
+    # Sets default terminal for Gnome .desktop shortcuts
+    # See: https://github.com/ublue-os/main/issues/211#issuecomment-1551600704
+    # Also see: https://discussion.fedoraproject.org/t/fedora-terminal-and-alternatives/106438
+    ".local/bin/xdg-terminal-exec".source = dotfiles/xdg-terminal-exec;
+
+    ".local/share/dbus-1/services/ca.desrt.dconf-editor.service".source =
+      dotfiles/dbus-services/ca.desrt.dconf-editor.service;
+    ".local/share/dbus-1/services/com.github.neithern.g4music.service".source =
+      dotfiles/dbus-services/com.github.neithern.g4music.service;
+    ".local/share/dbus-1/services/com.github.wwmm.easyeffects.service".source =
+      dotfiles/dbus-services/com.github.wwmm.easyeffects.service;
+    ".local/share/dbus-1/services/io.github.celluloid_player.Celluloid.service".source =
+      dotfiles/dbus-services/io.github.celluloid_player.Celluloid.service;
+    ".local/share/dbus-1/services/org.gnome.seahorse.Application.service".source =
+      dotfiles/dbus-services/org.gnome.seahorse.Application.service;
+    ".local/share/dbus-1/services/re.sonny.Tangram.service".source =
+      dotfiles/dbus-services/re.sonny.Tangram.service;
   };
 
   xdg.configFile = {
     # "gtk-4.0/assets".source = "${pkgs.orchis-theme}/share/themes/Orchis-Light-Compact/gtk-4.0/assets";
     # "gtk-4.0/gtk.css".source = "${pkgs.orchis-theme}/share/themes/Orchis-Light-Compact/gtk-4.0/gtk.css";
     "docker/config.json".source = dotfiles/docker-config.json;
-  };
-  
-  # If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  home.sessionVariables = {
-    # EDITOR = "emacs";
-    DOCKER_CONFIG = "${config.home.homeDirectory}/.config/docker";
-    NIXOS_OZONE_WL = "1";
-    SSH_AUTH_SOCK = "${config.home.homeDirectory}/.var/app/com.bitwarden.desktop/data/.bitwarden-ssh-agent.sock";
+    "wezterm/wezterm.lua".source =
+      config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.config/nix-config/dotfiles/wezterm.lua";
   };
 
   # Let Home Manager install and manage itself.
