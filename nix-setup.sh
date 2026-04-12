fa2f63a064b8f4849ee651f962bd0a896dec8461
winstonwumbo <me@winstonwumbo.com>
2026-04-12 16:34:23 -0400

    Automatically set up transient root for OSTree distros

--- nix-setup.sh
+++ nix-setup.sh
@@ -1,14 +1,31 @@
 #!/bin/bash
 
+# Makes / temporarily writable (for stuff like Nix installer)
+# https://github.com/coreos/rpm-ostree/issues/337#issuecomment-2856321727
+# https://github.com/DeterminateSystems/nix-installer/issues/1445#issuecomment-2856334377
+if [ -f /run/ostree-booted ]; then
+  if [ ! -f /etc/ostree/prepare-root.conf ]; then
+    sudo tee /etc/ostree/prepare-root.conf <<'EOL'
+[composefs]
+enabled = yes
+[root]
+transient = true
+EOL
+
+    rpm-ostree initramfs-etc --reboot --track=/etc/ostree/prepare-root.conf
+    printf "Enabled \033[34mtransient root\033[0m to support the \033[34mNix Store\033[0m on \033[34mOSTree\033[0m\n"
+    printf "Please \033[34mrestart\033[0m your device, then rerun \033[34mnix-setup.sh\033[0m\n"
+    return
+  fi
+fi
+
 # Initialize home-manager flake
 nix run home-manager/master -- init --switch
 
 # Move to correct directory
-cd ~/.config
+cd "$HOME"/.config || return
 
 # Remove home-manager presets
-rm -r home-manager/*
+rm -r home-manager/
 
-# Make symlinks
-ln -s -r nix-config/home.nix home-manager/home.nix
-ln -s -r nix-config/flake.nix home-manager/flake.nix
+home-manager switch --flake "$HOME"/.config/nix-config/
